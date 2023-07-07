894ede556d3098e9afaf98f3b074285ede642790
AjinkyaGY21 <136622844+AjinkyaGY21@users.noreply.github.com>
2023-07-07 23:49:48 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,15 @@
+public class Car implements Vehicle {
+    private VehicleType type;
+
+    public Car(VehicleType type) {
+        this.type = type;
+    }
+
+    public VehicleSize getSize() {
+        return VehicleSize.MEDIUM;
+    }
+
+    public VehicleType getType() {
+        return this.type;
+    }
+}
